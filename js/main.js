0c431f435f8b286333884babfc21840b7e9c5b2c
IffyDeathValley99 <47678294+IffyDeathValley99@users.noreply.github.com>
2024-02-08 15:15:27 -0700

    Testing for data storage strategies

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
 let namesList = ["George Washington", "Mahatma Ghandi", "God"];
+let namesListJapanese = ["ジョージ・ワシントン", "マハトマ・ガンジー", "神様"];
+let currentNames;
 let singularName;
+let language;
 function random(min, max) {
-    return Math.random() * (max - min) + min;
+    return Math.floor(Math.random() * (max - min) ) + min;
+}   
+function setLanguages () {
+    language = "Japanese";
+    currentNames = 0;
+
+    document.getElementById("")
 }
 function determineName () {
-    singularName = random(0, namesList.length - 1);
+    singularName = currentNames[random(0, currentNames.length - 1)];
     document.getElementById("nameOutput").textContent=singularName;
 }
+function studyLater () {
+
+}
+setLanguages(), determineName();
+
+
+let cheese;
+
+fetch('./data.json')
+.then(function(u){ return u.json();})
+.then(function(json){cheese = json;})
+
+console.log(cheese)
