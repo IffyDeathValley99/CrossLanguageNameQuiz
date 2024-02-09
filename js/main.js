f6ecec06a6508057c1b977b9a33d326c685668b1
IffyDeathValley99 <47678294+IffyDeathValley99@users.noreply.github.com>
2024-02-08 20:06:17 -0700

    Added Basic Functionality
    Everything but JSON parsing ):

--- js/main.js
+++ js/main.js
@@ -1,24 +1,62 @@
-let currentNames;
-let singularName;
+let namesList;
+let currentPeople = [
+    ["Mohatma Ghandi", "God"], 
+    ["マハトマ・ガンジー", "神様"], 
+    ["Description for Ghandi Soon?", "One of the gods of all time"]
+];
+
+let peopleIndex;
+let singularDescription;
+let englishName;
+let translatedName;
 let language;
+let studyList;
+
+let correctCounter = 0;
+let incorrectCounter = 0;
+
 function random(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
-}   
+}
 function setLanguages () {
     language = "Japanese";
-    currentNames = 0;
-
-    document.getElementById("")
+    // Set currentPeople
 }
-function determineName () {
-    singularName = currentNames[random(0, currentNames.length - 1)];
-    document.getElementById("nameOutput").textContent=singularName;
+function newName () {
+    peopleIndex = random(0, currentPeople.length - 1);
+    englishName = currentPeople[0][peopleIndex];
+    translatedName = currentPeople[1][peopleIndex];
+    singularDescription = currentPeople[2][peopleIndex];
+
+    document.getElementById("nameOutput").textContent=translatedName;
+    document.getElementById("descriptionOutput").textContent=singularDescription;
+}
+function checkGuess () {
+    var guess = ((document.getElementById("guessInput").value).toLowerCase()).replace(/\s+/g, '');
+    if (guess == (englishName.toLowerCase()).replace(/\s+/g, '')) {
+        newName();
+        correctCounter++;
+        document.getElementById("guessInput").value = "";
+    }
 }
 function studyLater () {
-
+    studyList.push(translatedName);
 }
-setLanguages(), determineName();
 
-const fs = require('fs');
-const jsonData = readFileSync('data.json', 'utf8');
-console.log(jsonData)
+// var jsonData;
+// fetch("./data.json", {
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json'
+//     }
+// }).then(function(res) {
+//     if(!res.ok) {
+//         console.error("Failed to fetch data.json"):
+//     }
+//     return res.json();
+// }).then(function(res) {
+//     jsonData = res;
+// }).catch(function(err) {
+//     console.log("FETCH ERROR: " + err);
+// });
+setLanguages(), newName();
